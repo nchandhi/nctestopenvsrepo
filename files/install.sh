a22d66b74a290e77ef712353d1c58a863b5b77ae
nchandhi <nalini.chandhi@microsoft.com>
2025-08-27 15:08:00 -0700

    script change

--- files/install.sh
+++ files/install.sh
@@ -1,3 +1,3 @@
 pip install -r requirements.txt --user
 
-git clone https://github.com/microsoft/Multi-Agent-Custom-Automation-Engine-Solution-Accelerator.git
+git clone https://github.com/microsoft/Multi-Agent-Custom-Automation-Engine-Solution-Accelerator.git .
