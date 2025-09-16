5ab797ea242d0aa8c66e1f9d3578cf5810a407d8
nchandhi <nalini.chandhi@microsoft.com>
2025-09-16 05:18:04 -0700

    folder name fix

--- files/install.sh
+++ files/install.sh
@@ -1,7 +1,13 @@
 pip install -r requirements.txt --user
 
-curl -L https://github.com/nchandhi/ncdatarepo1/archive/refs/heads/main.zip -o repo.zip
-unzip -o repo.zip -d .
-rm repo.zip
+
+curl -L https://github.com/nchandhi/ncdatarepo1/archive/refs/heads/main.zip -o repo.zip \
+  && unzip -q repo.zip -d temp \
+  && cp -r temp/ncdatarepo1-main/* . \
+  && rm -rf temp repo.zip
+
+# curl -L https://github.com/nchandhi/ncdatarepo1/archive/refs/heads/main.zip -o repo.zip
+# unzip -o repo.zip -d .
+# rm repo.zip
 
 # git clone https://github.com/microsoft/Multi-Agent-Custom-Automation-Engine-Solution-Accelerator.git .
