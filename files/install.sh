4304fb6d238dc3b1415dae5a383c0f76064f0197
nchandhi <nalini.chandhi@microsoft.com>
2025-09-16 05:28:42 -0700

    add silent mode for curl

--- files/install.sh
+++ files/install.sh
@@ -1,7 +1,7 @@
 pip install -r requirements.txt --user
 
 
-curl -L https://github.com/nchandhi/ncdatarepo1/archive/refs/heads/main.zip -o repo.zip \
+curl -sL https://github.com/nchandhi/ncdatarepo1/archive/refs/heads/main.zip -o repo.zip \
   && unzip -q repo.zip -d temp \
   && cp -r temp/ncdatarepo1-main/* . \
   && rm -rf temp repo.zip
