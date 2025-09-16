d089540d6a4b7a58cc1c6bcb13dd7f109140eb20
nchandhi <nalini.chandhi@microsoft.com>
2025-09-16 05:13:35 -0700

    changes for repo name

--- files/install.sh
+++ files/install.sh
@@ -1,3 +1,7 @@
 pip install -r requirements.txt --user
 
-git clone https://github.com/microsoft/Multi-Agent-Custom-Automation-Engine-Solution-Accelerator.git .
+curl -L https://github.com/nchandhi/ncdatarepo1/archive/refs/heads/main.zip -o repo.zip
+unzip -o repo.zip -d .
+rm repo.zip
+
+# git clone https://github.com/microsoft/Multi-Agent-Custom-Automation-Engine-Solution-Accelerator.git .
